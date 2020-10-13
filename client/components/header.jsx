d7ed53def09905c381e38bbf66dfd9ab6b2215e8
PaulKillian <psk65lava@gmail.com>
2020-10-13 10:24:54 -0700

    updates

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 function Header(props) {
   return (
     <div className="container d-flex flex-row justify-content-center color col-11 shadow mt-3 header-image">
-      <div className="col-7 d-flex flex-wrap justify-content-start mr-4">
-        <h2 className="mt-1 pl-5 text-white">The Super Wings Store</h2>
+      <div className="row d-flex flex-wrap justify-content-start mr-4">
+        <h2 className="row mt-1 ml-5 pl-5 text-white header">The Super Wings Store</h2>
       </div>
-      <h5 className="text-white mt-2 pointer" onClick={() => props.setView('cart', { params: {} })}>
+      <h5 className="text-white mt-2 ml-5 pointer" onClick={() => props.setView('cart', { params: {} })}>
         {props.items} Items <i className="fas fa-shopping-cart text-white mt-2"></i></h5>
     </div>
   );
